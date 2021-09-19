c92e4583b5976072740a080c968eaf256f6810bf
Robert Kaiser <kaiserrj@gmail.com>
2021-09-19 15:20:07 -0400

    Week 1 Day 5 exercies 1-4

--- module-1/05_Command_Line_Programs/exercise-student/dotnet/DecimalToBinary/Program.cs
+++ module-1/05_Command_Line_Programs/exercise-student/dotnet/DecimalToBinary/Program.cs
@@ -6,7 +6,18 @@ namespace DecimalToBinary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+                        
+            Console.Write("Please enter in a series of decimal values (separated by spaces): ");
+            string conversions = Console.ReadLine();
+            string[] conversionsArray = conversions.Split(' ');
+
+            for (int i = 0; i < conversionsArray.Length; i++)
+            {
+                decimal stringAsDecimal = decimal.Parse(conversionsArray[i]);
+                Console.WriteLine(conversionsArray[i] + " in binary is " + Convert.ToString((int)stringAsDecimal, 2));
+            }
+
+
         }
     }
 }
