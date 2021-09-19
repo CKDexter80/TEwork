673157f581412551b0f28d601659c22b480e650b
Robert Kaiser <kaiserrj@gmail.com>
2021-09-19 17:13:32 -0400

    Week 1 Day 5 submitted

--- module-1/05_Command_Line_Programs/exercise-student/dotnet/DecimalToBinary/Program.cs
+++ module-1/05_Command_Line_Programs/exercise-student/dotnet/DecimalToBinary/Program.cs
@@ -11,13 +11,39 @@ namespace DecimalToBinary
             string conversions = Console.ReadLine();
             string[] conversionsArray = conversions.Split(' ');
 
-            for (int i = 0; i < conversionsArray.Length; i++)
+            // short way
+            /*for (int i = 0; i < conversionsArray.Length; i++)
             {
                 decimal stringAsDecimal = decimal.Parse(conversionsArray[i]);
                 Console.WriteLine(conversionsArray[i] + " in binary is " + Convert.ToString((int)stringAsDecimal, 2));
+            }*/
+
+            // long way
+            for (int i = 0; i < conversionsArray.Length; i++)
+            {
+                decimal convertedStringToDecimal = decimal.Parse(conversionsArray[i]);
+                decimal dividend = convertedStringToDecimal;
+                int quotient = 0;
+                string binaryHolder = "";
+                string binaryDigit = "";
+
+                if (convertedStringToDecimal == 0)
+                {
+                    binaryHolder += "0";
+                }
+                else
+                {
+                    while (dividend > 0)
+                    {
+                        binaryDigit = Convert.ToString((int)dividend % 2);
+                        binaryHolder = binaryDigit + binaryHolder;
+                        quotient = (int)dividend / 2;
+                        dividend = quotient;
+                    }
+                }
+
+                Console.WriteLine(conversionsArray[i] + " in binary is " + binaryHolder);
             }
-
-
         }
     }
 }
