881dcebf8c442d0aa83dd351b1bfe123c0a2f949
Robert Kaiser <kaiserrj@gmail.com>
2021-09-22 11:41:56 -0400

    lecture exercise

--- module-1/08_Collections_Part_2/lecture-student/dotnet/CollectionsPart2Lecture/Program.cs
+++ module-1/08_Collections_Part_2/lecture-student/dotnet/CollectionsPart2Lecture/Program.cs
@@ -4,15 +4,70 @@ using System.Collections.Generic;
 namespace CollectionsPart2Lecture
 {
     public class CollectionsPart2Lecture
-	{
+    {
         static void Main(string[] args)
         {
-			Console.WriteLine("####################");
-			Console.WriteLine("       DICTIONARIES");
-			Console.WriteLine("####################");
-			Console.WriteLine();
+            Console.WriteLine("####################");
+            Console.WriteLine("       DICTIONARIES");
+            Console.WriteLine("####################");
+            Console.WriteLine();
+            //what kind of dictionary
+            //vending machine items
+            //key //value
+            Dictionary<int, string> contentsOfVM = new Dictionary<int, string>()
+            {	//keypad# //item at location
+				{512, "cheetos" },
+                {1024, "flaming hots" }
+            };
+            contentsOfVM.Add(4, "skittles");
+            contentsOfVM[8] = "skittles";
 
+            //changing values
 
-		}
-	}
+            string item = contentsOfVM[8];
+            contentsOfVM[8] = "KitKat";
+
+            //remove
+            bool didWeRemoveNotExists = contentsOfVM.Remove(555);
+            bool didWeRemoveExists = contentsOfVM.Remove(4);
+
+            //looping
+
+            foreach (KeyValuePair<int, string> content in contentsOfVM)
+            {
+                Console.WriteLine($"Our vending machine has {content.Value} at slot {content.Key}");
+            }
+
+            //HashSet
+
+            HashSet<string> coolProgrammingLanguages = new HashSet<string>();
+
+            coolProgrammingLanguages.Add("C#");
+            coolProgrammingLanguages.Add("C#"); //unique values
+            coolProgrammingLanguages.Add("c#");
+            coolProgrammingLanguages.Add("python");
+            coolProgrammingLanguages.Add("ruby");
+
+            HashSet<string> weirdProgrammingLanguages = new HashSet<string>() { "javascript", "ruby" };
+
+            //intersectsWith
+            //coolProgrammingLanguages.IntersectWith(weirdProgrammingLanguages);
+            //coolProgrammingLanguages = coolProgrammingLanguages;
+
+            //unionWith
+            //coolProgrammingLanguages.UnionWith(weirdProgrammingLanguages);
+            //coolProgrammingLanguages = coolProgrammingLanguages;
+
+            //exceptWith
+
+            coolProgrammingLanguages.ExceptWith(weirdProgrammingLanguages);
+            coolProgrammingLanguages = coolProgrammingLanguages;
+        }
+        //return type method name params??
+        static bool ContainsKey(Dictionary<int, string> dictionaryToCheck, int keyToCheck)
+        {
+            //contentsofVM are out of scope
+            return dictionaryToCheck.ContainsKey(keyToCheck);
+        }
+    }
 }
