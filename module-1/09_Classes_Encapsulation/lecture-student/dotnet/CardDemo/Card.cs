adf7689be1bd2596a7a689c221d0c6209cb46ae7
Robert Kaiser <kaiserrj@gmail.com>
2021-09-23 12:01:54 -0400

    lecture exercise

---
+++ module-1/09_Classes_Encapsulation/lecture-student/dotnet/CardDemo/Card.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardDemo
+{
+    class Card
+    {
+        //constructor
+        public Card(int value, string suit)
+        {
+            //this.Value = Value;
+            Value = value;
+            Suit = suit;
+
+            //Console.WriteLine("creating a new card");
+        }
+
+        public int Value { get; private set; }
+        
+        public string Suit { get; private set; }
+
+        public bool IsFaceUp { get; set;}
+        
+        public void Flip()
+        {
+            IsFaceUp = !IsFaceUp;
+
+            //if (IsFaceUp)
+            //{
+            //    IsFaceUp = false;
+            //}
+            //else
+            //{
+            //    IsFaceUp = true;
+            //}
+        }
+ 
+        public string Color
+        {
+            get
+            {
+                //method
+                if (Suit == "spades" || Suit == "clubs")
+                {
+                    return "black";
+                }
+                else
+                {
+                    return "red";
+                }
+
+            }
+        }
+
+    }
+    //no code; keep
+}
---
+++ module-1/09_Classes_Encapsulation/lecture-student/dotnet/CardDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CardDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creates an instance of card class
+            //casd is atype
+            Card card0 = new Card(2, "hearts");
+            card0.Flip();
+            Console.WriteLine($"Card card0 vale is {card0.Value}");
+            //card0.Value = 10;
+            //card0.Suit = "spades";
+            Console.WriteLine($"Card card0 color is {card0.Color}");
+            string color = card0.Suit;
+            //card0.Suit = "hearts";
+
+            Console.WriteLine($"Card card0 color is {card0.Color}");
+           
+            //Card card1 = new Card();
+
+
+
+            
+        }
+    }
+}
