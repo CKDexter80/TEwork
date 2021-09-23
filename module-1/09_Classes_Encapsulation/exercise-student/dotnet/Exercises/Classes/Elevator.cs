f320d18f6b78372da9c39500dd027b19e7927ab3
Robert Kaiser <kaiserrj@gmail.com>
2021-09-23 16:14:39 -0400

    Week 2 Day 4 Submitted

--- module-1/09_Classes_Encapsulation/exercise-student/dotnet/Exercises/Classes/Elevator.cs
+++ module-1/09_Classes_Encapsulation/exercise-student/dotnet/Exercises/Classes/Elevator.cs
@@ -2,7 +2,51 @@
 {
     public class Elevator
     {
+        public Elevator (int numberOfLevels)
+        {
+            NumberOfLevels = numberOfLevels;
+        }
 
+        public int CurrentLevel { get; private set; } = 1;
+        public int NumberOfLevels { get; private set; }
+        public bool DoorIsOpen { get; private set; }
+
+        public void OpenDoor()
+        {
+            DoorIsOpen = true;
+        }
+        public void CloseDoor()
+        {
+            DoorIsOpen = false;
+        }
+        public void GoUp(int desiredFloor)
+        {
+            if (!DoorIsOpen)
+            {
+                if (desiredFloor > CurrentLevel)
+                {
+                    if (desiredFloor <= NumberOfLevels)
+                    {
+                        CurrentLevel = desiredFloor;
+                    }
+                }
+            }
+            
+        }
+        public void GoDown(int desiredFloor)
+        {
+            if (!DoorIsOpen)
+            {
+                if (desiredFloor > 1 && desiredFloor < CurrentLevel)
+                {
+                    CurrentLevel = desiredFloor;
+                }
+                else
+                {
+                    CurrentLevel = 1;
+                }
+            }
+        }
 
     }
 }
--- module-1/09_Classes_Encapsulation/exercise-student/dotnet/Exercises/Classes/Television.cs
+++ module-1/09_Classes_Encapsulation/exercise-student/dotnet/Exercises/Classes/Television.cs
@@ -2,6 +2,88 @@
 {
     public class Television
     {
+        public bool IsOn { get; private set; } = false;
+        public int CurrentChannel { get; private set; } = 3;
+        public int CurrentVolume { get; private set; } = 2;
+
+        public void TurnOff()
+        {
+            IsOn = false;
+        }
+        public void TurnOn()
+        {
+            IsOn = true;
+            CurrentChannel = 3;
+            CurrentVolume = 2;
+
+        }
+        public void ChangeChannel(int newChannel)
+        {
+            if (IsOn)
+            {
+                if (newChannel >= 3 || newChannel <= 18)
+                {
+                    CurrentChannel = newChannel;
+                }
+            }
+        }
+        public void ChannelUp()
+        {
+            if (IsOn)
+            {
+                if (CurrentChannel == 18)
+                {
+                    CurrentChannel = 3;
+                }
+                else
+                {
+                    CurrentChannel++;
+                }
+            }
+        }
+        public void ChannelDown()
+        {
+            if (IsOn)
+            {
+                if (CurrentChannel == 3)
+                {
+                    CurrentChannel = 18;
+                }
+                else
+                {
+                    CurrentChannel--;
+                }
+            }
+        }
+        public void RaiseVolume()
+        {
+            if (IsOn)
+            {
+                if (CurrentVolume > 0 && CurrentVolume < 10)
+                {
+                    CurrentVolume++;
+                }
+                else
+                {
+                    CurrentVolume = CurrentVolume;
+                }
+            }
+        }
+        public void LowerVolume()
+        {
+            if (IsOn)
+            {
+                if (CurrentVolume > 0 && CurrentVolume < 10)
+                {
+                    CurrentVolume--;
+                }
+                else
+                {
+                    CurrentVolume = CurrentVolume;
+                }
+            }
+        }
+
 
 
     }
