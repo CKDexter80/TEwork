3c6b420abd37282ae8cb997058fc1c742eeb45ea
Robert Kaiser <kaiserrj@gmail.com>
2021-09-23 14:39:23 -0400

    Week 2 Day 4 exercises 1-3

--- module-1/09_Classes_Encapsulation/exercise-student/dotnet/Exercises/Classes/FruitTree.cs
+++ module-1/09_Classes_Encapsulation/exercise-student/dotnet/Exercises/Classes/FruitTree.cs
@@ -2,7 +2,26 @@
 {
     public class FruitTree
     {
+        public FruitTree(string typeOfFruit, int startingPiecesOfFruit)
+        {
+            TypeOfFruit = typeOfFruit;
+            PiecesOfFruitLeft = startingPiecesOfFruit;
+        }
 
+        public string TypeOfFruit { get; private set; }
+        public int PiecesOfFruitLeft { get; private set; }
 
+        public bool PickFruit(int numberOfPiecesToRemove)
+        {
+            if (PiecesOfFruitLeft - numberOfPiecesToRemove >= 0)
+            {
+                PiecesOfFruitLeft -= numberOfPiecesToRemove;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
--- module-1/09_Classes_Encapsulation/exercise-student/dotnet/Exercises/Classes/HomeworkAssignment.cs
+++ module-1/09_Classes_Encapsulation/exercise-student/dotnet/Exercises/Classes/HomeworkAssignment.cs
@@ -2,7 +2,50 @@
 {
     public class HomeworkAssignment
     {
+        public HomeworkAssignment(int possibleMarks, string submitterName)
+        {
+            PossibleMarks = possibleMarks;
+            SubmitterName = submitterName;
+        }
 
+        public int EarnedMarks { get; set; }
+        public int PossibleMarks { get; private set; }
+        public string SubmitterName { get; private set; }
 
+        public int GradeAsInt
+        {
+            get
+            {
+                return (int)((double)EarnedMarks / (double)PossibleMarks * 100);
+            }
+        }
+        
+        public string LetterGrade {
+            get
+            {
+                if (GradeAsInt  >= 90 )
+                {
+                    return "A";
+                }
+                else if (GradeAsInt <= 89 && GradeAsInt >= 80)
+                {
+                    return "B";
+                }
+                else if (GradeAsInt <= 79 && GradeAsInt >= 70)
+                {
+                    return "C";
+                }
+                else if (GradeAsInt <= 69 && GradeAsInt >= 60)
+                {
+                    return "D";
+                }
+                else
+                {
+                    return "F";
+                }
+            }
+        }
+
+        
     }
 }
