d2ceb4a6cdac8608a46d6e1a99780bd04fb6d3e1
Robert Kaiser <kaiserrj@gmail.com>
2021-09-27 15:18:35 -0400

    Week 3 Day 1 submitted

--- module-1/11_Inheritance/exercise-student/dotnet/BankTellerExercise/Classes/CheckingAccount.cs
+++ module-1/11_Inheritance/exercise-student/dotnet/BankTellerExercise/Classes/CheckingAccount.cs
@@ -3,30 +3,24 @@
     public class CheckingAccount : BankAccount
     {
         
-        public CheckingAccount(string accountHolderName, string accountNumber) : base(accountHolderName, accountNumber)
-        {
+        public CheckingAccount(string accountholdername, string accountnumber) : base(accountholdername, accountnumber) {}
 
-        }
-
-        public CheckingAccount(string accountHolderName, string accountNumber, decimal balance) : base(accountHolderName, accountNumber, balance)
-        {
-
-        }
+        public CheckingAccount(string accountHolderName, string accountNumber, decimal balance) : base(accountHolderName, accountNumber, balance) {}
 
         public override decimal Withdraw(decimal amountToWithdraw)
         {
-            if (base.Balance - amountToWithdraw > -100M)
+            if (Balance - amountToWithdraw > -100M)
             {
-                if (base.Balance - amountToWithdraw < 0M)
+                if (Balance - amountToWithdraw < 0M)
                 {
-                    base.Balance = (base.Balance -= amountToWithdraw) - 10M;
+                    Balance = (Balance -= amountToWithdraw) - 10M;
                 }
                 else
                 {
-                    base.Balance -= amountToWithdraw;
+                    Balance -= amountToWithdraw;
                 }
             }
-            return base.Balance;
+            return Balance;
         }
     }
 }
--- module-1/11_Inheritance/exercise-student/dotnet/BankTellerExercise/Classes/SavingsAccount.cs
+++ module-1/11_Inheritance/exercise-student/dotnet/BankTellerExercise/Classes/SavingsAccount.cs
@@ -3,29 +3,23 @@
     public class SavingsAccount : BankAccount
     {
 
-        public SavingsAccount(string accountHolderName, string accountNumber) : base(accountHolderName, accountNumber)
-        {
+        public SavingsAccount(string accountHolderName, string accountNumber) : base(accountHolderName, accountNumber)  {}
 
-        }
-
-        public SavingsAccount(string accountHolderName, string accountNumber, decimal balance) : base(accountHolderName, accountNumber, balance)
-        {
-
-        }
+        public SavingsAccount(string accountHolderName, string accountNumber, decimal balance) : base(accountHolderName, accountNumber, balance) {}
 
         public override decimal Withdraw(decimal amountToWithdraw)
         {
-            if (!(base.Balance - amountToWithdraw - 2M < 0M))
+            if (!(Balance - amountToWithdraw - 2M < 0M))
             {
-                if (base.Balance - amountToWithdraw < 150M)
+                if (Balance - amountToWithdraw < 150M)
                 {
-                    base.Balance -= amountToWithdraw + 2M;
+                    Balance -= amountToWithdraw + 2M;
                 }
                 else
                 {
-                    base.Balance -= amountToWithdraw;
+                    Balance -= amountToWithdraw;
                 }
-            } return base.Balance;
+            } return Balance;
         }
     }
 }
