88fc4180a029a61041b9908f323efd18d0c8bc5b
Robert Kaiser <kaiserrj@gmail.com>
2021-09-28 14:45:27 -0400

    Week 3 Day 2 submitted

---
+++ module-1/12_Polymorphism/exercise-student/dotnet/BankTellerExercise/BankCustomer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankTellerExercise
+{
+    public class BankCustomer : IAccountable
+    {
+        public decimal Balance { get; private set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsVip
+        {
+            get
+            {
+                decimal totalBalance = 0;
+                foreach (IAccountable account in accountsToDate)
+                {
+                    totalBalance += account.Balance;
+                }
+                if (totalBalance >= 25000M)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        private List<IAccountable> accountsToDate = new List<IAccountable>();
+
+        public void AddAccount(IAccountable newAccount)
+        {
+            accountsToDate.Add(newAccount);
+        }
+
+        public IAccountable[] GetAccounts()
+        {
+            IAccountable[] accounts = accountsToDate.ToArray();
+            return accounts;
+        }
+    }
+}
---
+++ module-1/12_Polymorphism/exercise-student/dotnet/BankTellerExercise/CreditCardAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankTellerExercise
+{
+    public class CreditCardAccount: IAccountable
+    {
+        public CreditCardAccount(string accountHolderName, string accountNumber)
+        {
+            AccountHolderName = accountHolderName;
+            AccountNumber = accountNumber;
+        }
+
+        public decimal Balance { get; private set; }
+        public string AccountHolderName { get; private set; }
+        public string AccountNumber { get; }
+        public decimal Debt { get; private set; }
+
+        public decimal Pay (decimal amountToPay)
+        {
+            Debt -= amountToPay;
+            return Balance = -Debt;
+        }
+
+        public decimal Charge (decimal amountToCharge)
+        {
+            Debt += amountToCharge;
+            return Balance = -Debt;
+
+        }
+    }
+}
