c2dc412756430a578184bfa14c3a3c3f91e5ad73
Robert Kaiser <kaiserrj@gmail.com>
2021-09-29 16:47:21 -0400

    Week 3 Day 3 submitted

---
+++ module-1/13_Inheritance_Abstract_Classes/exercise-student/dotnet/Exercise/Wall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise
+{
+    public abstract class Wall
+    {
+        public Wall(string name, string color)
+        {
+            Name = name;
+            Color = color;
+        }
+
+        public string Name { get;}
+        public string Color { get; }
+
+        public abstract int GetArea();
+
+       // public abstract string ToString();
+        
+    }
+}
