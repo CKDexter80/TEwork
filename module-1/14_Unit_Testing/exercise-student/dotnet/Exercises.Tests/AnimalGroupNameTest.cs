1372c3ccdac3c92f55bd0a61e7befe4732f2df14
Robert Kaiser <kaiserrj@gmail.com>
2021-10-01 10:43:32 -0400

    Week 3 Day 4

---
+++ module-1/14_Unit_Testing/exercise-student/dotnet/Exercises.Tests/AnimalGroupNameTest.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class AnimalGroupNameTest
+    {
+        [TestMethod]
+        public void GetHerdWithLowercaseString()
+        {
+            AnimalGroupName animalGroupName = new AnimalGroupName();
+
+            string inputL = "giraffe";
+
+            string expectedL = animalGroupName.GetHerd(inputL);
+            string actualL = "Tower";
+
+            Assert.AreEqual(expectedL, actualL);
+        }
+
+        [TestMethod]
+        public void GetHerdWithUppercaseString()
+        {
+            AnimalGroupName animalGroupName = new AnimalGroupName();
+
+            string inputU = "FLAMINGO";
+
+            string expectedU = animalGroupName.GetHerd(inputU);
+            string actualU = "Pat";
+
+            Assert.AreEqual(expectedU, actualU);
+        }
+        [TestMethod]
+        public void GetHerdWithMixedcaseString()
+        {
+            AnimalGroupName animalGroupName = new AnimalGroupName();
+
+            string inputM = "Rhino";
+
+            string expectedM = animalGroupName.GetHerd(inputM);
+            string actualM = "Crash";
+
+            Assert.AreEqual(expectedM, actualM);
+        }
+        [DataTestMethod]
+        [DataRow("walrus", "unknown")]
+        [DataRow("elephants", "unknown")]        
+        public void GetHerdWithNotFound(string input, string returned)
+        {
+            AnimalGroupName animalGroupName = new AnimalGroupName();
+
+            string inputNF = input;
+            string actualNF = animalGroupName.GetHerd(inputNF);
+            string expectedNF = returned;
+
+            Assert.AreEqual(expectedNF, actualNF);
+        }
+        [TestMethod]
+        public void GetHerdWithNull()
+        {
+            AnimalGroupName animalGroupName = new AnimalGroupName();
+
+            string inputN = null;
+
+            string expectedN = animalGroupName.GetHerd(inputN);
+            string actualN = "unknown";
+
+            Assert.AreEqual(expectedN, actualN);
+        }
+        [TestMethod]
+        public void GetHerdWithEmpty()
+        {
+            AnimalGroupName animalGroupName = new AnimalGroupName();
+
+            string inputE = "";
+
+            string expectedE = animalGroupName.GetHerd(inputE);
+            string actualE = "unknown";
+
+            Assert.AreEqual(expectedE, actualE);
+        }
+    }
+}
---
+++ module-1/14_Unit_Testing/exercise-student/dotnet/Exercises.Tests/CigarPartyTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+
+    public class CigarPartyTest
+    {
+        [TestMethod]
+        public void HavePartyOnWeekDayWithFewerThanFortyCigars()
+        {
+            CigarParty cigarParty = new CigarParty();
+
+            int cig = 30;
+            bool isW = false;
+            bool expected = false;
+            bool actual = cigarParty.HaveParty(cig, isW);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void HavePartyOnWeekDayWithMoreThanFortyCigarsAndFewerThanSixty()
+        {
+            /*
+             haveParty(30, false) → false
+        haveParty(50, false) → true
+        haveParty(70, true) → true
+            */
+            CigarParty cigarParty = new CigarParty();
+
+            int ciga = 50;
+            bool isWe = false;
+            bool expectedW = true;
+            bool actualW = cigarParty.HaveParty(ciga, isWe);
+
+            Assert.AreEqual(expectedW, actualW);
+        }
+        [TestMethod]
+        public void HavePartyOnWeekEndWithMoreThanFortyCigars()
+        {
+            /*
+             haveParty(30, false) → false
+        haveParty(50, false) → true
+        haveParty(70, true) → true
+            */
+            CigarParty cigarParty = new CigarParty();
+
+            int cigar = 50;
+            bool isWee = false;
+            bool expectedW = true;
+            bool actualW = cigarParty.HaveParty(ciga, isWe);
+
+            Assert.AreEqual(expectedW, actualW);
+
+        }
+    }
+}
