6190af4fe6a8d981be02d13eae10b3deb7cb45be
Robert Kaiser <kaiserrj@gmail.com>
2021-10-06 11:03:25 -0400

    lecture exercise

---
+++ module-1/18_Review/lecture-student/Code-O-Ween/Code-O-Ween/Code-O-Ween/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Code_O_Ween
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
