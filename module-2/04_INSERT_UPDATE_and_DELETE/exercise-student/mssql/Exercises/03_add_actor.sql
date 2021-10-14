30a2482736b4d3d500a333d0f44eab0879de393e
Robert Kaiser <kaiserrj@gmail.com>
2021-10-14 14:24:05 -0400

    WEEK 5 Day 4 submitted

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/mssql/Exercises/03_add_actor.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/mssql/Exercises/03_add_actor.sql
@@ -1,2 +1,7 @@
 -- 3. Did you know Eric Stoltz was originally cast as Marty McFly in "Back to the Future"? Add Eric Stoltz to the list of actors for "Back to the Future" (1 row)
 
+INSERT INTO movie_actor (movie_id, actor_id)
+VALUES (
+	(SELECT m.movie_id FROM movie m WHERE title = 'Back to the Future'),
+	(SELECT p.person_id FROM person p WHERE p.person_name = 'Eric Stoltz')
+	);
--- module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/mssql/Exercises/04_add_genre.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/mssql/Exercises/04_add_genre.sql
@@ -1,2 +1,10 @@
 -- 4. Add a "Sports" genre to the genre table. Add the movie "Coach Carter" to the newly created genre. (1 row each)
 
+INSERT INTO genre (genre_name)
+VALUES ('Sports');
+
+INSERT INTO movie_genre (movie_id, genre_id)
+VALUES (
+	(SELECT m.movie_id FROM movie m WHERE m.title = 'Coach Carter'),
+	(SELECT g.genre_id FROM genre g WHERE g.genre_name = 'Sports')
+	);
