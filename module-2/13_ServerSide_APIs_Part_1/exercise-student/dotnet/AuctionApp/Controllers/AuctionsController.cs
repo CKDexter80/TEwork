9c85143caa0e78b56dca15fad7344cb980d3e7c0
Robert Kaiser <kaiserrj@gmail.com>
2021-10-27 13:27:19 -0400

    Week 7 Day 3 submitted

--- module-2/13_ServerSide_APIs_Part_1/exercise-student/dotnet/AuctionApp/Controllers/AuctionsController.cs
+++ module-2/13_ServerSide_APIs_Part_1/exercise-student/dotnet/AuctionApp/Controllers/AuctionsController.cs
@@ -22,5 +22,39 @@ namespace AuctionApp.Controllers
                 dao = auctionDao;
             }
         }
+
+        [HttpGet]
+        public List<Auction> GetAuctionList(string title_like = "", double currentBid_lte = 0)
+        {
+            if (title_like != "" && currentBid_lte != 0)
+            {
+                return dao.SearchByTitleAndPrice(title_like, currentBid_lte);
+            }
+
+            else if (title_like != "")
+            {
+                return dao.SearchByTitle(title_like);
+            }
+            else if (currentBid_lte != 0)
+            {
+                return dao.SearchByPrice(currentBid_lte);
+            }
+
+
+            return dao.List();
+        }
+
+        [HttpGet("{auctionId}")]
+        public Auction GetAuctionById(int auctionId)
+        {
+            return dao.Get(auctionId);
+        }
+
+        [HttpPost]
+        public Auction CreateAuction(Auction auction)
+        {
+            return dao.Create(auction);
+        }
+        
     }
 }
