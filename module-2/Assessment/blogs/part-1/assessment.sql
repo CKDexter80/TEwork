9df10572e3da2152f5733407798aa8061deae7f3
Robert Kaiser <kaiserrj@gmail.com>
2021-11-02 10:32:25 -0400

    assessment

--- module-2/Assessment/blogs/part-1/assessment.sql
+++ module-2/Assessment/blogs/part-1/assessment.sql
@@ -2,11 +2,33 @@ USE assessment;
 GO
 
 -- Select all columns from posts that are published
+SELECT p.id, p.name, p.body, p.published, p.created
+FROM posts p
+WHERE p.published = 1;
 
 -- Select posted_by and body from comments that have a body and were created after Oct. 15, 2019
 
+SELECT c.posted_by, c.body
+FROM comments c
+WHERE c.body IS NOT NULL
+	AND c.created > '10/15/2019';
+
 -- Select the name and published states from posts ordered by when they were created, earliest first
+
+SELECT p.name, p.published, p.created
+FROM posts p
+ORDER BY p.created ASC;
 
 -- Select the created date and the count of all the comments created on that date
 
+SELECT c.created, COUNT(*) AS number_of_comments
+FROM comments c
+GROUP BY c.created
+ORDER BY c.created ASC;
+
 -- Select the post name, comment posted_by and comment body for all posts created after Oct. 1st, 2019
+
+SELECT p.name, c.posted_by, c.body
+FROM posts p
+JOIN comments c ON p.id = c.post_id
+WHERE p.created > '10/1/2019';
--- module-2/Assessment/blogs/part-2/Blogs/Blogs/PostSqlDao.cs
+++ module-2/Assessment/blogs/part-2/Blogs/Blogs/PostSqlDao.cs
@@ -15,14 +15,74 @@ namespace Blogs
 
         public IList<Post> GetAllPosts()
         {
-            // Implement this method to pull in all posts from database
+            IList<Post> allPosts = new List<Post>();
 
-            return null;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string sql = "SELECT p.id, p.name, p.body, p.published, p.created " +
+                        "FROM posts p";
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Post post = new Post();
+
+                        post.Body = Convert.ToString(reader["body"]);
+                        post.Created = Convert.ToDateTime(reader["created"]);
+                        post.Id = Convert.ToInt64(reader["id"]);
+                        post.IsPublished = Convert.ToBoolean(reader["published"]);
+                        post.Name = Convert.ToString(reader["name"]);
+
+                        allPosts.Add(post);
+                    }
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return allPosts;
         }
 
         public void Save(Post newPost)
         {
-            // Implement this method to save post to database
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string sql = "INSERT INTO posts (name, body, published, created) " +
+                        "VALUES(@name, @body, @published, @created)";
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+
+                    
+                    cmd.Parameters.AddWithValue("@name", newPost.Name);
+                    cmd.Parameters.AddWithValue("@body", newPost.Body);
+                    cmd.Parameters.AddWithValue("@published", newPost.IsPublished);
+                    cmd.Parameters.AddWithValue("@created", newPost.Created);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
