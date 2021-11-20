ac8083c0463416ace017aeff1c8a1d9d12a5da3f
Robert Kaiser <kaiserrj@gmail.com>
2021-11-20 15:39:26 -0500

    Week 10 Day 5 submitted

--- module-3/11_Introduction_to_Vue_and_Data_Binding/exercise-student/src/components/UserList.vue
+++ module-3/11_Introduction_to_Vue_and_Data_Binding/exercise-student/src/components/UserList.vue
@@ -1,22 +1,46 @@
 <template>
   <table id="tblUsers">
     <thead>
-    <tr>
+      <tr>
         <th>First Name</th>
         <th>Last Name</th>
         <th>Username</th>
         <th>Email Address</th>
         <th>Status</th>
-    </tr>
+      </tr>
     </thead>
     <tbody>
       <tr>
-        <td><input type="text" id="firstNameFilter"/></td>
-        <td><input type="text" id="lastNameFilter"/></td>
-        <td><input type="text" id="usernameFilter"/></td>
-        <td><input type="text" id="emailFilter"/></td>
         <td>
-          <select id="statusFilter">
+          <input
+            v-model.trim="filter.firstName"
+            type="text"
+            id="firstNameFilter"
+          />
+        </td>
+        <td>
+          <input
+            v-model.trim="filter.lastName"
+            type="text"
+            id="lastNameFilter"
+          />
+        </td>
+        <td>
+          <input
+            v-model.trim="filter.username"
+            type="text"
+            id="usernameFilter"
+          />
+        </td>
+        <td>
+          <input
+            v-model.trim="filter.emailAddress"
+            type="text"
+            id="emailFilter"
+          />
+        </td>
+        <td>
+          <select v-model="filter.status" id="statusFilter">
             <option value="">Show All</option>
             <option value="Active">Active</option>
             <option value="Disabled">Disabled</option>
@@ -24,35 +48,135 @@
         </td>
       </tr>
       <!-- user listing goes here -->
+      <tr
+        v-for="user in filteredList"
+        v-bind:key="user.username"
+        v-bind:class="{ disabled: user.status === 'Disabled' }"
+      >
+        <td>{{ user.firstName }}</td>
+        <td>{{ user.lastName }}</td>
+        <td>{{ user.username }}</td>
+        <td>{{ user.emailAddress }}</td>
+        <td>{{ user.status }}</td>
+      </tr>
     </tbody>
   </table>
 </template>
 
 <script>
 export default {
-  name: 'user-list',
+  name: "user-list",
   data() {
     return {
       users: [
-        { firstName: 'John', lastName: 'Smith', username: 'jsmith', emailAddress: 'jsmith@gmail.com', status: 'Active' },
-        { firstName: 'Anna', lastName: 'Bell', username: 'abell', emailAddress: 'abell@yahoo.com', status: 'Active' },
-        { firstName: 'George', lastName: 'Best', username: 'gbest', emailAddress: 'gbest@gmail.com', status: 'Disabled' },
-        { firstName: 'Ben', lastName: 'Carter', username: 'bcarter', emailAddress: 'bcarter@gmail.com', status: 'Active' },
-        { firstName: 'Katie', lastName: 'Jackson', username: 'kjackson', emailAddress: 'kjackson@yahoo.com', status: 'Active' },
-        { firstName: 'Mark', lastName: 'Smith', username: 'msmith', emailAddress: 'msmith@foo.com', status: 'Disabled' }
-      ]
-    }
-  }
-}
+        {
+          firstName: "John",
+          lastName: "Smith",
+          username: "jsmith",
+          emailAddress: "jsmith@gmail.com",
+          status: "Active",
+        },
+        {
+          firstName: "Anna",
+          lastName: "Bell",
+          username: "abell",
+          emailAddress: "abell@yahoo.com",
+          status: "Active",
+        },
+        {
+          firstName: "George",
+          lastName: "Best",
+          username: "gbest",
+          emailAddress: "gbest@gmail.com",
+          status: "Disabled",
+        },
+        {
+          firstName: "Ben",
+          lastName: "Carter",
+          username: "bcarter",
+          emailAddress: "bcarter@gmail.com",
+          status: "Active",
+        },
+        {
+          firstName: "Katie",
+          lastName: "Jackson",
+          username: "kjackson",
+          emailAddress: "kjackson@yahoo.com",
+          status: "Active",
+        },
+        {
+          firstName: "Mark",
+          lastName: "Smith",
+          username: "msmith",
+          emailAddress: "msmith@foo.com",
+          status: "Disabled",
+        },
+      ],
+
+      filter: {
+        firstName: "",
+        lastName: "",
+        username: "",
+        emailAddress: "",
+        status: "",
+      },
+    };
+  },
+
+  computed: {
+    filteredList() {
+      let list = this.users;
+
+      if (this.filter.status != "") {
+        list = list.filter((user) => user.status === this.filter.status);
+      }
+
+      if (this.filter.firstName != "") {
+        list = list.filter((user) =>
+          user.firstName
+            .toLowerCase()
+            .includes(this.filter.firstName.toLowerCase())
+        );
+      }
+
+      if (this.filter.lastName != "") {
+        list = list.filter((user) =>
+          user.lastName
+            .toLowerCase()
+            .includes(this.filter.lastName.toLowerCase())
+        );
+      }
+
+      if (this.filter.username != "") {
+        list = list.filter((user) =>
+          user.username
+            .toLowerCase()
+            .includes(this.filter.username.toLowerCase())
+        );
+      }
+
+      if (this.filter.emailAddress != "") {
+        list = list.filter((user) =>
+          user.emailAddress
+            .toLowerCase()
+            .includes(this.filter.emailAddress.toLowerCase())
+        );
+      }
+
+      return list;
+    },
+  },
+};
 </script>
 
 <style scoped>
 table {
   margin-top: 20px;
-  font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
 th {
-  text-transform: uppercase
+  text-transform: uppercase;
 }
 td {
   padding: 10px;
@@ -60,7 +184,8 @@ td {
 tr.disabled {
   color: red;
 }
-input, select {
+input,
+select {
   font-size: 16px;
 }
 </style>
